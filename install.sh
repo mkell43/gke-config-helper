97c6c49ed4b01e9fd5ff8aa75ce1e65e95ea01b0
Mike Keller <mkeller@betterview.com>
2022-05-01 15:59:40 -0700

    Init install script.

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+OS="$(uname -s | awk '{print tolower($0)}')"
+export OS
+ARCH="$(arch)"
+
+if [ "$ARCH" = "x86_64" ]; then
+  ARCH="amd64"
+fi
+export ARCH
+
+mkdir -p "$HOME"/bin
+
+curl https://github.com/mkell43/gke-config-helper/releases/latest/download/gke-config-helper_"${OS}"_"${ARCH}" \
+  --output "$HOME"/bin/gke-config-helper \
+  --silent
+
+chmod +x "$HOME"/bin/gke-config-helper
